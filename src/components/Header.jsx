2f7c9bacdd85133bdb3660948e3a3a776a788cad
Altin Abdurahmani <aa29333@seeu.edu.mk>
2024-11-05 13:12:15 +0000

    Deleted src

--- src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="fixed top-0 left-0 w-full bg-gray-800 text-white p-4 z-10 shadow-md">
-      <h1 className="text-xl font-semibold">Transactions</h1>
-    </header>
-  );
-}
